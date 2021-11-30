b35548524e043eb20423d3dad9d3bfe573d6ee89
EsQLap <61599652+EsQLap@users.noreply.github.com>
2021-11-30 23:55:57 +0400

    Добавлен метод getAll с пагинацией

--- src/main/java/com/bolsheviks/APMS/domain/User/UserRepository.java
+++ src/main/java/com/bolsheviks/APMS/domain/User/UserRepository.java
@@ -1,5 +1,7 @@
 package com.bolsheviks.APMS.domain.User;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     int countUserById(UUID id);
     Optional<User> findFirstByLoginAndPassword(String login, String password);
+
+    Page<User> findAllByIdNotOrderByLastName(UUID uuid, Pageable pageable);
 }
